666f130b5730673f87cea138151f91a316c8b754
Trevor <tahlers@unomaha.edu>
2025-02-22 21:28:54 -0600

    This is just the login page. i am still working on the api

---
+++ SchedulerApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';  
+import { DataService } from './home/data.service';
+import { AppComponent } from './app.component';  
+import { HomeComponent } from './home/home.component';  
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component'; 
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
--- SchedulerApp/src/app/app.routes.ts
+++ SchedulerApp/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component'; 
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
