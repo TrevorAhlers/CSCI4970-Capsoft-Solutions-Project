881196c5a629633f6505127c40d5a651b6f458e9
HarborUser <hareshjayaraman21@gmail.com>
2025-03-03 22:40:27 -0600

    Added Home page, and slighly modified the login page

--- SchedulerApp/src/app/app.routes.ts
+++ SchedulerApp/src/app/app.routes.ts
@@ -12,4 +12,6 @@ export const routes: Routes = [
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
   })
-  export class AppRoutingModule { }
+  export class AppRoutingModule {
+    
+   }
