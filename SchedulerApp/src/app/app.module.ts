e938cff933dc357ff398b172ff51d646147376ed
HarborUser <hareshjayaraman21@gmail.com>
2025-03-15 22:11:46 -0500

    added outline/template page, and wrote a test cases

--- SchedulerApp/src/app/app.module.ts
+++ SchedulerApp/src/app/app.module.ts
@@ -5,9 +5,16 @@ import { DataService } from './home/data.service';
 import { AppComponent } from './app.component';  
 import { HomeComponent } from './home/home.component';  
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component'; 
-
-
+import { LoginComponent } from './login/login.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
+import { MatCard } from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {MDCRipple} from '@material/ripple';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatDividerModule} from '@angular/material/divider'
+import {MatIconModule} from '@angular/material/icon';
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent }
@@ -21,9 +28,23 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatCard,
+    MatButtonModule,
+    MatTabsModule,
+    MatDividerModule,
+    MatIconModule
+    
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA // Add this line
+  ]
+
 })
 
 export class AppModule { }
