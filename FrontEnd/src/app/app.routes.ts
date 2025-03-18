38caf13b8c455c484e8a88432e80dc5ad0a0dcca
sambot900 <106410947+sambot900@users.noreply.github.com>
2025-03-17 21:52:24 -0500

    -updated layout (html/css)

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { DataComponent } from './services/data.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
 	{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'data', component: DataComponent },
   { path: '**', redirectTo: 'login' }
