01bcd42262223ab65a9118f12165343fc7380d03
hshanno <hshanno@unomaha.edu>
2025-04-19 21:48:11 -0500

    added edit tab, added labels and fields to edit tab

--- FrontEnd/src/app/home/details/details.component.ts
+++ FrontEnd/src/app/home/details/details.component.ts
@@ -1,13 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
+
+
 export class DetailsComponent {
   @Input() selectedCourse: any;
   courseDetailsHtml: string | null = null;
+  userInput1: string = '';
+  userInput2: string = '';
+  userInput3: string = '';
+  userInput4: string = '';
+  userInput5: string = '';
+  userInput6: string = '';
+  userInput7: string = '';
+  userInput8: string = '';
+  userInput9: string = '';
+  userInput10: string = '';
 
   ngOnChanges(): void {
     if (this.selectedCourse) {
