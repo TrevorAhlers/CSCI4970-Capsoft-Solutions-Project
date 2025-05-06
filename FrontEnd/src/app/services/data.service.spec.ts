e9eb4b9254ecb84747cb87f1db4fdf2b6a8a3ea6
HarborUser <118377981+HarborUser@users.noreply.github.com>
2025-05-06 18:28:26 -0500

    Update data.service.spec.ts

--- FrontEnd/src/app/services/data.service.spec.ts
+++ FrontEnd/src/app/services/data.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
-
 import { DataService } from './data.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('DataService', () => {
-	let service: DataService;
+  let service: DataService;
+  let httpTestingController: HttpTestingController;
 
-	beforeEach(() => {
-		TestBed.configureTestingModule({});
-		service = TestBed.inject(DataService);
-	});
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DataService],
+    });
 
-	it('should be created', () => {
-		expect(service).toBeTruthy();
-	});
+    service = TestBed.inject(DataService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify(); 
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should get data', () => {
+    const mockData = "";
+     
+    expect(service.getData()).toBeTruthy()
+    service.getData().subscribe((data) => {
+      expect(data).toBe(mockData);
+      close();
+    });
+
+    const httpRequest = httpTestingController.expectOne('http://localhost:5000/api/data');
+    expect(httpRequest.request.method).toBe('GET');
+    
+   
+  });
 });
